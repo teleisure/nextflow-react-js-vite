1cf7ff424f802ff2a85a827ec274d0f791a3723b
teleisure <95207606+teleisure@users.noreply.github.com>
2025-07-02 09:05:51 +0000

    feat: update Hello component and change danger class color to dark red

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,39 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+  const Hello = (props) => {
+
+    if (props.username === undefined) {
+      return <p>username</p>
+    }
+    return (
+      <>
+      <div className='danger'>
+        <h1>Hello World {props.username}</h1>
+      </div>
+      </>
+    )
+  }
+
+  function Goodbye() {
+    return (
+      <div>
+        <h1>Goodbye World</h1>
+      </div>
+    )
+  }
+
 function App() {
   let city = "Bangkok"
+  const onClickMe = () => {
+    alert("Button clicked!")
+  }
   return (
     <div>
-      <h1 id="header" className='danger'>Hello {city}</h1>
+      <Hello username={city}/>
+      <Hello></Hello>
+      <Goodbye />
+      <button onClick={onClickMe}>Click me!!!</button>
     </div>
   )
 
